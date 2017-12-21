0366533fcfbced97d9b61b423a162cf77b6e1cf2
Saintenr <bosold.leonard@gmail.com>
2017-12-21 18:37:04 +0100

    neu

--- www/controller/Customer.controller.js
+++ www/controller/Customer.controller.js
@@ -1,10 +1,10 @@
-﻿sap.ui.define([sap.ui.define([
+sap.ui.define([
     "hs/fulda/customer/management/controller/BaseController",
     "jquery.sap.global",
     "sap/m/Button",
-    "sap/m/Dialog",
+	"sap/m/Dialog",
     "sap/m/Input",
-    "sap/m/Text",
+	"sap/m/Text",
     "sap/ui/model/json/JSONModel"
 ], function (BaseController, JQuery, Button, Dialog, Input, Text, JSONModel) {
     "use strict";
@@ -14,7 +14,7 @@
         /**
          * Lifecycle Method which is called at the first start of the view
          */
-        onInit: function () {
+        onInit: function(){
             // Set style class cozy
             this.getView().addStyleClass("cozy");
 
@@ -28,17 +28,17 @@
          * Is called everytime the route gets matched
          * @private
          */
-        _onObjectMatched: function (oEvent) {
-            this.iCampaignId = oEvent.getParameter("arguments").CampaignId - 1;
-            this.itemBindingPath = "/Campaigns/" + this.iCampaignId + "/Customer";
+        _onObjectMatched: function(oEvent){
+            this.iCampaignId = oEvent.getParameter("arguments").CampaignId-1;
+            this.itemBindingPath = "/Campaigns/"+ this.iCampaignId +"/Customer";
             // Bind Campaign Name
-            this.getView().byId("customerTitle").bindElement("/Campaigns/" + this.iCampaignId);
-            //            // Read actual campaign object
-            //            var oCampaign = this.getView().getModel().getProperty("/Campaigns/"+this.iCampaignId);
-            //            // Get the title from the object
-            //            var sTitle = oCampaign.CampaignName;
-            //            // Set the header title
-            //            this.getView().byId("customerTitle").setText(sTitle);
+            this.getView().byId("customerTitle").bindElement("/Campaigns/"+this.iCampaignId);
+//            // Read actual campaign object
+//            var oCampaign = this.getView().getModel().getProperty("/Campaigns/"+this.iCampaignId);
+//            // Get the title from the object
+//            var sTitle = oCampaign.CampaignName;
+//            // Set the header title
+//            this.getView().byId("customerTitle").setText(sTitle);
             // Bind Customer List
             this.getView().byId("customerList").bindAggregation("items", {
                 path: this.itemBindingPath,
@@ -49,45 +49,45 @@
         /**
          *
          */
-        onCustomerItemPressed: function () {
+        onCustomerItemPressed: function(){
             var oItem = oEvent.getSource();
-            var oRouter = this.getRouter();
+			var oRouter = this.getRouter();
             var sPath = oItem.getBindingContext().getPath();
             var object = oItem.getModel().getProperty(sPath);
             //console.log(object.CampaignId);
 
-            oRouter.navTo("CustomerDetail");
-            //                          ", {
-            //				CampaignId: object.CampaignId
-            //			});
+			oRouter.navTo("CustomerDetail");
+//                          ", {
+//				CampaignId: object.CampaignId
+//			});
         },
 
         /**
          *  Edit Campaign
          */
-        onEdit: function (oEvent) {
+        onEdit: function(oEvent){
             this.editDialog = this.getView().byId("");
             // Read actual campaign object
-            var oCampaign = this.getView().getModel().getProperty("/Campaigns/" + this.iCampaignId);
+            var oCampaign = this.getView().getModel().getProperty("/Campaigns/"+this.iCampaignId);
             console.log(oCampaign);
             var oCampaignData = new JSONModel(oCampaign);
             // Set binding mode
             oCampaignData.setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
 
             if (!this.editDialog) {
-                this.editDialog = sap.ui.xmlfragment("hs.fulda.customer.management.fragment.EditCampaign", this);
+				this.editDialog = sap.ui.xmlfragment("hs.fulda.customer.management.fragment.EditCampaign", this);
 
-                //to get access to the global model
-                this.getView().addDependent(this.editDialog);
-            }
+				//to get access to the global model
+				this.getView().addDependent(this.editDialog);
+			}
             this.editDialog.setModel(oCampaignData);
             this.editDialog.bindElement("/");
             // toggle compact style
-            jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this.editDialog);
-            this.editDialog.open();
+			jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this.editDialog);
+			this.editDialog.open();
         },
 
-        onSaveCampaign: function (sCampaignName) {
+        onSaveCampaign: function(sCampaignName){
             var navCon = this.getView().byId("navContainerCampaignList");
             // Save Data persistent
             var JSONData = this.getView().getModel().getJSON();
@@ -97,25 +97,25 @@
             this.getOwnerComponent().getModel().refresh(true);
         },
 
-        /**
-        *
-        */
-        onCloseDialog: function (oEvent) {
+         /**
+         *
+         */
+        onCloseDialog: function(oEvent){
             console.log(oEvent);
 
         },
 
-        onDialogOk: function (oEvent) {
+        onDialogOk: function(oEvent){
 
         },
 
-        onCapturePhoto: function (oEvent) {
-            if (!navCon) {
+        onCapturePhoto: function(oEvent){
+            if(!navCon){
                 var navCon = this.getView().byId("navContainerCustomerList");
             }
             var target = oEvent.getSource().data("target");
 
-            if (!oPage) {
+            if(!oPage){
                 var oPage = this.getView().byId(target);
             }
 
@@ -125,19 +125,19 @@
             myhtml.setContent(myPhotoArea);
             oPage.addContent(myhtml);
 
-            if (target) {
-                var animation = "show";
-                navCon.to(this.getView().byId(target), animation);
-            } else {
-                navCon.back();
-            }
+			if (target) {
+				var animation = "show";
+				navCon.to(this.getView().byId(target), animation);
+			} else {
+				navCon.back();
+			}
 
             //navigator.device.capture.captureVideo(captureSuccess, captureError, {limit: 1});
             var oCamera = navigator.camera;
             oCamera.getPicture(this.onCaptureSuccess, this.onCaptureError, { quality: 10, destinationType: oCamera.DestinationType.DATA_URL });
         },
 
-        onCaptureSuccess: function () {
+        onCaptureSuccess: function(){
             var i, path, len;
             for (i = 0, len = mediaFiles.length; i < len; i += 1) {
                 path = mediaFiles[i].fullPath;
@@ -146,82 +146,82 @@
             }
         },
 
-        onCaptureError: function (e) {
-            console.log("capture error: " + JSON.stringify(e));
+        onCaptureError: function(e){
+            console.log("capture error: "+JSON.stringify(e));
         },
 
         /**
          *
          */
-        onAddCustomerManual: function (oEvent) {
+        onAddCustomerManual: function(oEvent){
             this.getView().byId("statusErrorCreateCustomer").setVisible(false);
             this.getView().byId("statusInformationCreateCustomer").setVisible(false);
 
-            if (!navCon) {
+           if(!navCon){
                 var navCon = this.getView().byId("navContainerCustomerList");
             }
-            var target = oEvent.getSource().data("target");
-            if (target) {
-                var animation = "show";
-                navCon.to(this.getView().byId(target), animation);
-            } else {
-                navCon.back();
-            }
+			var target = oEvent.getSource().data("target");
+			if (target) {
+				var animation = "show";
+				navCon.to(this.getView().byId(target), animation);
+			} else {
+				navCon.back();
+			}
         },
 
-        onSaveCustomer: function (oEvent) {
+        onSaveCustomer: function(oEvent){
 
-            if (!oModel) {
+            if(!oModel){
                 var oModel = this.getView().getModel();
             }
             console.log("error");
-            //   var oNewCustomerData = {};
+         //   var oNewCustomerData = {};
             var oNewCustomerData = {
-                "CustomerName": "",
-                "Company": ""
+                "CustomerName" : "",
+                "Company" : ""
             };
 
-            if (!oInputCustomerName) {
+            if(!oInputCustomerName){
                 var oInputCustomerName = this.getView().byId("inputCustomerName");
             }
             var sCustomerName = oInputCustomerName.getValue();
 
-            if (!oInputCustomerCompany) {
+            if(!oInputCustomerCompany){
                 var oInputCustomerCompany = this.getView().byId("inputCustomerCompany");
             }
             var sCustomerCompany = oInputCustomerCompany.getValue();
 
-            if (sCustomerCompany === null || sCustomerCompany === undefined || sCustomerCompany === "" ||
-                sCustomerName === null || sCustomerName === undefined || sCustomerName === "") {
+            if(sCustomerCompany === null || sCustomerCompany === undefined || sCustomerCompany === ""  ||
+                sCustomerName === null || sCustomerName === undefined || sCustomerName === ""){
                 this.getView().byId("statusInformationCreateCustomer").setVisible(true);
             } else {
                 // Set data
                 oNewCustomerData.CustomerName = sCustomerName;
                 oNewCustomerData.Company = sCustomerCompany;
 
-                if (!oModel.getProperty("/Campaigns/" + this.iCampaignId + "/Customer")) {
-                    var bValueSet = oModel.setProperty("/Campaigns/" + this.iCampaignId + "/Customer");
+                if(!oModel.getProperty("/Campaigns/"+ this.iCampaignId +"/Customer")){
+                    var bValueSet = oModel.setProperty("/Campaigns/"+ this.iCampaignId +"/Customer");
 
-                    if (bValueSet === true) {
+                    if(bValueSet === true){
                         var aCustomer = [];
                     }
                 } else {
-                    var aCustomer = oModel.getProperty("/Campaigns/" + this.iCampaignId + "/Customer");
+                    var aCustomer = oModel.getProperty("/Campaigns/"+ this.iCampaignId +"/Customer");
                 }
                 // Push data to array
                 aCustomer.push(oNewCustomerData);
                 // Check status
-                var bResponse = oModel.setProperty("/Campaigns/" + this.iCampaignId + "/Customer", aCustomer);
+                var bResponse = oModel.setProperty("/Campaigns/"+ this.iCampaignId +"/Customer", aCustomer);
                 // Error handling
-                if (bResponse === true) {
+                if(bResponse === true){
                     this.onCustomerCreateSuccess(sCustomerName);
                 } else {
                     this.getView().byId("statusErrorCreateCustomer").setVisible(true);
                 }
-            }
+             }
         },
 
-        onCustomerCreateSuccess: function (sCustomerName) {
+        onCustomerCreateSuccess: function(sCustomerName){
             var navCon = this.getView().byId("navContainerCustomerList");
             // Save Data persistent
             var JSONData = this.getView().getModel().getJSON();
@@ -233,21 +233,21 @@
             this.getView().byId("inputCustomerCompany").setValue("");
         },
 
-        onCancelCustomer: function (oEvent) {
+        onCancelCustomer: function(oEvent){
             // Clear inout fields
             this.getView().byId("inputCustomerName").setValue("");
             this.getView().byId("inputCustomerCompany").setValue("");
             // Nav Back
-            if (!navCon) {
+            if(!navCon){
                 var navCon = this.getView().byId("navContainerCustomerList");
             }
-            var target = oEvent.getSource().data("target");
-            if (target) {
-                var animation = "show";
-                navCon.to(this.getView().byId(target), animation);
-            } else {
-                navCon.back();
-            }
+			var target = oEvent.getSource().data("target");
+			if (target) {
+				var animation = "show";
+				navCon.to(this.getView().byId(target), animation);
+			} else {
+				navCon.back();
+			}
         }
-    });
+	});
 });
