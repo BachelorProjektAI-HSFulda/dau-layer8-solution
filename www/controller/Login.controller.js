7c95da409f3edc88321a48601ed8b016e50b4ea8
Saintenr <bosold.leonard@gmail.com>
2017-12-22 15:55:48 +0100

    22.12.17 add google login

--- www/controller/Login.controller.js
+++ www/controller/Login.controller.js
@@ -20,14 +20,31 @@ sap.ui.define([
         },
 
         /**
-         *
+         * START: handle Google+ or Facebook login
          */
         onLoginPress: function(oEvent){
 
             var sProvider = oEvent.getSource().data("provider");
             switch(sProvider) {
                 case "google":
-                    // Login to google - todo implementation
+                    window.plugins.googleplus.login(
+                        {
+                          'scopes': '', // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
+                          'webClientId': '855269254217-5n7v4d9vqujeh6kn8cuoopab9ue5q02j.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+                          'offline': false, // optional, but requires the webClientId - if set to true the plugin will also return a serverAuthCode, which can be used to grant offline access to a non-Google server
+                        },
+                        function (obj) {
+                            // obj Array Objekte:
+                            // accessToken, displayName, email, familyName, givenName, idToken, imageUrl, refreshToken, serverAuthCode, userID
+
+                            alert(JSON.stringify("Hallo " + obj.displayName + " du hast dich erfolgreich angemeldet."));
+                            var sSecToken = obj.accessToken;
+                            console.log(sSecToken);
+                        },
+                        function (msg) {
+                            alert('error: ' + msg);
+                        }
+                    );
                     break;
                 case "facebook":
                     CordovaFacebook.login({
@@ -59,7 +76,7 @@ sap.ui.define([
         /**
          *
          */
-        onLoginPressed: function(oEvent){
+        t: function(oEvent){
             // Read input values
             var sUserName = this.getView().byId("idUserName").getValue();
             var sPassword = this.getView().byId("idPassword").getValue();
@@ -108,7 +125,20 @@ sap.ui.define([
               console.log('User signed out.');
             });
         },
-
+        /**
+         *
+         */
+        googleLogout: function() {
+            window.plugins.googleplus.logout(
+                function (msg) {
+                  document.querySelector("#image").style.visibility = 'hidden';
+                  document.querySelector("#feedback").innerHTML = msg;
+                },
+                function (msg) {
+                  document.querySelector("#feedback").innerHTML = msg;
+                }
+            );
+        },
         /**
          *
          */
